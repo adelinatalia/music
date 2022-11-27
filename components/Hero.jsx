5711874619e16abb39dda1a3e0dbadb197ed7a6f
Natalia <onataliadelina@gmail.com>
2022-11-27 21:23:45 +0100

    update hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,17 +3,24 @@ import imgHero from '@/img/hero.png'
 
 export default function Hero() {
     return (
-        <div>
-            <h1>Discover digital Art and collect <span className='gradient-text'> Musik </span> Platform</h1>
-            <p>Entdecke Musikneuheiten sowie im Trend liegende Alben und erstelle deine eigene Musiksammlung.</p>
-            <button className='btn-main'>entdecke</button>
-            <button className='btn-outline'>erstelle playlist</button>
+        <div className='hero flex-row'>
+            <div className='flex-column'>
+                <div className="hero__heading flex-column">
+                    <h1>Discover digital Art and collect <span className='gradient-text'> Musik </span> Platform</h1>
+                    <p className='hero__text'>Entdecke Musikneuheiten sowie im Trend liegende Alben und erstelle deine eigene Musiksammlung.</p>
+                </div>
+                <div className="hero__buttons flex-row">
+                    <button className='btn-primary hero__btn'>entdecke</button>
+                    <button className='btn-outline hero__btn'>erstelle playlist</button>
+                </div>
+            </div>
             <Image
                 alt="Hero"
                 src={imgHero}
                 width="450"
                 height="468"
                 placeholder="blur"
+                className='hero__image'
             />
         </div>
     )
