d6099d29face9be85ea8cb9764e39bfc19220d3e
Natalia <onataliadelina@gmail.com>
2022-11-28 22:23:40 +0100

    add songs

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -6,21 +6,24 @@ export default function Navigation() {
             <Link href="/">
                 <img src="./icons/logo.png" alt="logo" width="32" />
             </Link>
-            <Link href="/">home</Link>
-            <Link href="/">artists</Link>
-            <Link href="/">albums</Link>
-            <Link href="/">tracks</Link>
-            {/* <Link href="/">playlist</Link> */}
-            <Link href="/">lyrics</Link>
 
-            <form className='site-navigation__form '>
+            <div className='site-navigation__main-items'>
+                {/* <Link href="/">home</Link> */}
+                <Link href="/songs">songs</Link>
+                <Link href="/">artists</Link>
+                <Link href="/">albums</Link>
+                <Link href="/">tracks</Link>
+                {/* <Link href="/">playlist</Link> */}
+                <Link href="/">lyrics</Link>
+            </div>
+            {/* <form className='site-navigation__form '>
                 <label htmlFor="search" className='flex-row-center'>
                     <input type="text" id='search' placeholder='suche nach ...' />
                     <Link href="/" className='flex-row-center'>
                         <img src="./icons/icon-search.png" alt="search icon" width="30" />
                     </Link>
                 </label>
-            </form>
+            </form> */}
 
             <div className="site-navigation__btns">
                 <Link href="/" className='site-navigation__btn'>
---
+++ pages/songs.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout';
+import Songs from '@/components/Songs';
+
+export default function Home() {
+    return (
+        <Layout title="Songs">
+            <div>
+                <Songs />
+            </div>
+        </Layout>
+    )
+}
